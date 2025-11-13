9173e1021eca7e6b13807786902bbc8faf7379b0
kyaw-hmue-San <6731503062@lamduan.mfu.ac.th>
2025-11-13 16:51:07 +0700

    adding docker image

--- backend/src/main/java/com/elearn/lms/LmsELearnApplication.java
+++ backend/src/main/java/com/elearn/lms/LmsELearnApplication.java
@@ -3,11 +3,16 @@ package com.elearn.lms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class LmsELearnApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LmsELearnApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(LmsELearnApplication.class);
+        String port = System.getenv("PORT");
+        if (port != null && !port.isBlank()) {
+            app.setDefaultProperties(Collections.singletonMap("server.port", port));
+        }
+        app.run(args);
+    }
 }
